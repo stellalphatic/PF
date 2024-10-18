7d50a533b5b464ea81d0fa86f9b21e64dee91e7c
stellalphatic <163044429+stellalphatic@users.noreply.github.com>
2024-10-18 21:18:03 +0500

    week-6

--- PD/week-6/task02_op.cpp
+++ PD/week-6/task02_op.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
 using namespace std;
+void upperTriangle(int number){
+for (int i = number; i > 0; i--)
+    {
+        for (int j = 0; j < number; j++)
+        {
+            if (j < i)
+                cout << " ";
+            else
+                cout << "*";
+        }
+        cout << endl;
+    }
+}
+void lowerTriangle(int number){
+     for (int i = 0; i < number; i++)
+    {
+        for (int j = 0; j < number; j++)
+        {
+            if (j < i)
+                cout << " ";
+            else
+                cout << "*";
+        }
+        cout << endl;
+    }
+}
 int main()
 {
-    int n = 10;
-    for (int i = n; i > 0; i--)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (j < i)
-                cout << " ";
-            else
-                cout << "*";
-        }
-
-        cout << endl;
-    }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (j < i)
-                cout << " ";
-            else
-                cout << "*";
-        }
-
-        cout << endl;
-    }
+    int number = 10;
+    upperTriangle(number);
+    lowerTriangle(number);
+   
     return 0;
 }
